cbbd3eb2ce372f7273590e392bb0bee864241de8
vnshas <163927528+vnshas@users.noreply.github.com>
2025-04-17 14:48:02 -0300

    feat: adicionado Routes e route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,12 @@
+import { Navigate, Route, Routes } from "react-router-dom"
 
 
-export {}
+export const AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
+        </Routes>
+    )
+}
